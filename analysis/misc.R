dcdd083245b79291375142acee9b4bcf3aa50580
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-06-06 16:42:33 -0400

    noodling on combining revenue and expenses

--- analysis/misc.R
+++ analysis/misc.R
@@ -1,65 +1,21 @@
 source("load.R")
 
-sched_40_groups <- c(
-  "General government",
-  "Protection services",
-  "Transportation services",
-  "Environmental services",
-  "Health services",
-  "Social and family services",
-  "Social Housing",
-  "Recreation and cultural services",
-  "Planning and development"
-)
 
-ottawa_sched_40_2018 <- returns %>%
-  filter(marsyear == 2018) %>%
-  filter(municipality_desc == "Ottawa C") %>%
-  filter(schedule_desc == "CONSOLIDATED STATEMENT OF OPERATIONS: EXPENSES") %>%
+
+
+
+
+returns %>%
+  filter(slc %in% c("slc.80D.L1710.C07.01", "slc.80D.L1720.C07.01")) %>%
   remove_extra_columns() %>%
-  mutate(is_subtotal_entry = schedule_line_desc %in% sched_40_groups) %>% ## TODO: do this with detect 99 in `slc` instead
-  mutate(expense_group = ifelse(is_subtotal_entry, schedule_line_desc, NA_character_)) %>%
-  select(expense_group, everything()) %>%
-  fill(expense_group, .direction = "up") %>%
-  mutate(is_total_entry = schedule_line_desc == "Total")
-
-ottawa_sched_40_2018
-
-ottawa_sched_40_2018 %>%
-  filter(! is_subtotal_entry & ! is_total_entry) %>%
-  filter(schedule_column_desc == "Total Expenses After Adjustments") %>%
-  mutate(spend_pct = amount / sum(amount)) %>%
-  select(schedule_line_desc, spend = amount, spend_pct) %>%
-  arrange(-spend_pct)
-
-
-
-
-sched_40_2018 <- returns %>%
-  filter(marsyear == 2018) %>%
-  filter(schedule_desc == "CONSOLIDATED STATEMENT OF OPERATIONS: EXPENSES") %>%
-  remove_extra_columns() %>%
-  mutate(is_subtotal_entry = schedule_line_desc %in% sched_40_groups) %>% ## TODO: do this with detect 99 in `slc` instead
-  mutate(expense_group = ifelse(is_subtotal_entry, schedule_line_desc, NA_character_)) %>%
-  group_by(municipality_desc) %>%
-  select(expense_group, everything()) %>%
-  fill(expense_group, .direction = "up") %>%
-  mutate(is_total_entry = schedule_line_desc == "Total")
-
-sched_40_2018 %>% filter(tier_code == "ST") %>%
-  filter(! is_subtotal_entry & ! is_total_entry) %>%
-  filter(schedule_column_desc == "Total Expenses After Adjustments") %>%
-  mutate(spend_pct = amount / sum(amount)) %>%
-  select(schedule_line_desc, spend = amount, spend_pct) %>%
-  arrange(-spend_pct) %>%
-  filter(municipality_desc %in%
-           (sched_40_2018 %>%
-              filter(schedule_line_desc == "Police") %>%
-              select(municipality_desc) %>%
-              unique() %>%
-              pull()
-            )
-         ) %>%
-  filter(schedule_line_desc == "Police") %>%
-  View()
+  select(municipality_desc, marsyear, schedule_line_desc, amount) %>%
+  pivot_wider(names_from = schedule_line_desc, values_from = amount) %>%
+  clean_names() %>%
+  rename(
+    paved_km = roads_total_paved_lane_km,
+    paved_good_km = condition_of_roads_number_of_paved_lane_kilometres_where_the_con
+  ) %>%
+  #filter(! is.na(paved_km) & ! is.na(paved_good_km)) %>%
+  arrange(municipality_desc, marsyear) %>%
+  mutate(pct_good = paved_good_km / paved_km)
 
---
+++ analysis/police.R
@@ -0,0 +1,102 @@
+sched_40_groups <- c(
+  "General government",
+  "Protection services",
+  "Transportation services",
+  "Environmental services",
+  "Health services",
+  "Social and family services",
+  "Social Housing",
+  "Recreation and cultural services",
+  "Planning and development"
+)
+
+ottawa_sched_40_2018 <- returns %>%
+  filter(marsyear == 2018) %>%
+  filter(municipality_desc == "Ottawa C") %>%
+  filter(schedule_desc == "CONSOLIDATED STATEMENT OF OPERATIONS: EXPENSES") %>%
+  remove_extra_columns() %>%
+  mutate(is_subtotal_entry = schedule_line_desc %in% sched_40_groups) %>% ## TODO: do this with detect 99 in `slc` instead
+  mutate(expense_group = ifelse(is_subtotal_entry, schedule_line_desc, NA_character_)) %>%
+  select(expense_group, everything()) %>%
+  fill(expense_group, .direction = "up") %>%
+  mutate(is_total_entry = schedule_line_desc == "Total")
+
+ottawa_sched_40_2018
+
+ottawa_sched_40_2018 %>%
+  filter(! is_subtotal_entry & ! is_total_entry) %>%
+  filter(schedule_column_desc == "Total Expenses After Adjustments") %>%
+  mutate(spend_pct = amount / sum(amount)) %>%
+  select(schedule_line_desc, spend = amount, spend_pct) %>%
+  arrange(-spend_pct)
+
+
+
+
+sched_40_2018 <- returns %>%
+  filter(marsyear == 2018) %>%
+  filter(schedule_desc == "CONSOLIDATED STATEMENT OF OPERATIONS: EXPENSES") %>%
+  remove_extra_columns() %>%
+  mutate(is_subtotal_entry = schedule_line_desc %in% sched_40_groups) %>% ## TODO: do this with detect 99 in `slc` instead
+  mutate(expense_group = ifelse(is_subtotal_entry, schedule_line_desc, NA_character_)) %>%
+  group_by(municipality_desc) %>%
+  select(expense_group, everything()) %>%
+  fill(expense_group, .direction = "up") %>%
+  mutate(is_total_entry = schedule_line_desc == "Total")
+
+sched_40_2018 %>% filter(tier_code == "ST") %>%
+  filter(! is_subtotal_entry & ! is_total_entry) %>%
+  filter(schedule_column_desc == "Total Expenses After Adjustments") %>%
+  mutate(spend_pct = amount / sum(amount)) %>%
+  select(schedule_line_desc, spend = amount, spend_pct) %>%
+  arrange(-spend_pct) %>%
+  filter(municipality_desc %in%
+           (sched_40_2018 %>%
+              filter(schedule_line_desc == "Police") %>%
+              select(municipality_desc) %>%
+              unique() %>%
+              pull()
+           )
+  ) %>%
+  filter(schedule_line_desc == "Police") %>%
+  View()
+
+
+## TODO: make sure the categories actually apply properly here... currently they don't
+sched_12_2018 <- returns %>%
+  filter(marsyear == 2018) %>%
+  filter(schedule_desc == "GRANTS, USER FEES AND SERVICE CHARGES") %>%
+  remove_extra_columns() %>%
+  mutate(is_subtotal_entry = schedule_line_desc %in% sched_40_groups) %>% ## TODO: do this with detect 99 in `slc` instead
+  mutate(expense_group = ifelse(is_subtotal_entry, schedule_line_desc, NA_character_)) %>%
+  group_by(municipality_desc) %>%
+  select(expense_group, everything()) %>%
+  fill(expense_group, .direction = "up") %>%
+  mutate(is_total_entry = schedule_line_desc == "Total")
+
+sched_40_2018_toronto <- sched_40_2018 %>%
+  filter(municipality_desc == "Toronto C") %>%
+  ungroup() %>%
+  remove_extra_columns()
+
+sched_12_2018_toronto <- sched_12_2018 %>%
+  filter(municipality_desc == "Toronto C") %>%
+  ungroup() %>%
+  remove_extra_columns()
+
+
+toronto_revenue <- sched_12_2018_toronto %>%
+  filter(! is_subtotal_entry & ! is_total_entry) %>%
+  group_by(expense_group, schedule_line_desc) %>%
+  summarize(revenue = sum(amount, na.rm = TRUE)) %>%
+  ungroup()
+
+toronto_spend <- sched_40_2018_toronto %>%
+  filter(! is_subtotal_entry & ! is_total_entry) %>%
+  group_by(expense_group, schedule_line_desc) %>%
+  summarize(expense = sum(amount, na.rm = TRUE)) %>%
+  mutate(expense = expense * -1) %>%
+  ungroup()
+  
+toronto_spend %>%
+  left_join(toronto_revenue)
